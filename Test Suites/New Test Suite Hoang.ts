ba67c1151fecc644981bea8b3bc62f9103530e20
Hồ Huy Hoàng <hoangho1147@gmail.com>
2022-02-16 13:15:26 +0700

    addd

---
+++ Test Suites/New Test Suite Hoang.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite Hoang</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>57f5c8c6-0a13-41e3-8e70-43d9293620da</testSuiteGuid>
+</TestSuiteEntity>
